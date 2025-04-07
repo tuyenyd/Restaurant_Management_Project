3a669a71f1e5e3260834a664797a6979e36e752f
thanhnt004 <125938855+thanhnt004@users.noreply.github.com>
2025-04-07 20:58:39 +0700

    a

--- src/main/java/com/restaurant/restaurant_management_project/database/DatabaseConnection.java
+++ src/main/java/com/restaurant/restaurant_management_project/database/DatabaseConnection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.restaurant.restaurant_management_project.database;
+//package com.restaurant.restaurant_management_project.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=RM_Db;username=sa;password=123456;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
     private static Connection connection = null;
-    
+
     public static Connection GetConnection(){
         try{
             connection = DriverManager.getConnection(URL);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.restaurant.restaurant_management_project {
     requires javafx.controls;
     requires javafx.fxml;
     requires annotations;
+    requires java.sql;
 
     exports com.restaurant.restaurant_management_project;
     exports com.restaurant.restaurant_management_project.controller to javafx.fxml;
