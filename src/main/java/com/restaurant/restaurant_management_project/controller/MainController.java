82045eb6055ebe05652c3675bd5fa0d72d8c2535
tuyenyd <nguyenvantuyen12000@gmail.com>
2025-04-13 14:00:49 +0700

    Add files via upload

---
+++ src/main/java/com/restaurant/restaurant_management_project/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.example.tuyenyd.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+import java.io.IOException;
+
+public class MainController {
+    @FXML
+    private StackPane contentPane;
+
+    @FXML
+    public void showTableManagement() {
+        loadView("/com/example/tuyenyd/views/table-management.fxml");
+    }
+
+    @FXML
+    public void showBookingView() {
+        loadView("/com/example/tuyenyd/views/booking-view.fxml");
+    }
+    @FXML
+    public void showStatisticsView() {
+        loadView("/com/example/tuyenyd/views/booking-statistics.fxml");
+    }
+
+    private void loadView(String fxmlPath) {
+        try {
+            Parent view = FXMLLoader.load(getClass().getResource(fxmlPath));
+            contentPane.getChildren().clear();
+            contentPane.getChildren().add(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Không thể tải view: " + fxmlPath);
+        }
+    }
+}
